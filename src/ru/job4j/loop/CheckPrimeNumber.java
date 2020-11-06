d62e52f4877bcb9465c564b133c3f89cd46c16c6
Anton-Protasov <89267016232@mail.ru>
2020-11-07 01:38:03 +0300

    5.5. Простое число [#395158]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+/*
+5.5. Простое число [#395158]
+В этом задании нужно проверить, что число является простым.
+Простым считается число, которое делится только на себя и на единицу.
+ */
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        } else {
+            for (int integer = 2; integer <= (number - 1); integer++) {
+                if ((number % integer) == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+}
