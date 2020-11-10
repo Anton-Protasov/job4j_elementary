8d618d197df19675381f442bc0642f24fe1df6e1
Anton-Protasov <89267016232@mail.ru>
2020-11-10 21:14:04 +0300

    6.0.2. Заполнение массива. [#395079]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен surnames: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Anton";
+        System.out.println(names[0]);
+        names[1] = "Boris";
+        System.out.println(names[1]);
+        names[2] = "Vadim";
+        System.out.println(names[2]);
+        names[3] = "Galina";
+        System.out.println(names[3]);
     }
 }
