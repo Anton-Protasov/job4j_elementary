f80a3c67fd3aa769a9e325639c03a819c05593b5
Anton-Protasov <89267016232@mail.ru>
2020-11-11 22:18:11 +0300

    6.2.3. Перевернуть массив. [#395082]

---
+++ src/ru/job4j/array/ru/job4j/array/Turn.java
@@ -0,0 +1,16 @@
+package ru.job4j.array.ru.job4j.array;
+
+/*
+6.2.3. Перевернуть массив. [#395082]
+ */
+
+public class Turn {
+    public static int[] back(int[] array) {
+        for (int i = 0; i < (array.length / 2); i++) {
+            int var = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = var;
+        }
+        return array;
+    }
+}
