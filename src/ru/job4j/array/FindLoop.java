bd792b59f43ac253becbe71d66c71dcfe08900c3
Anton-Protasov <89267016232@mail.ru>
2020-11-15 00:35:34 +0300

    6.5. Поиск индекса в диапазоне. [#395072]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 /*
 6.1. Классический поиск перебором. [#395064]
+6.5. Поиск индекса в диапазоне. [#395072]
  */
 
 public class FindLoop {
@@ -16,6 +17,22 @@ public class FindLoop {
                 break;
             }
         }
+        return rst;
+    }
+/*
+Поиск индекса в диапазоне
+ */
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+            for (int i = start; i <= finish; i++) {
+                if (data[i] == el) {
+                    rst = i;
+                    break;
+                } else {
+                    rst = -1;
+                }
+            }
         return rst;
     }
 }
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -17,4 +17,37 @@ public class FindLoopTest {
         int[] input = new int[] {1, 3, 5, 7, 9};
         assertThat(FindLoop.indexOf(input, 6), is(-1));
     }
+
+    @Test
+    public void whenFind() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 10;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNotFind() {
+        int[] input = new int[] {5, 2, 0, 2, 4};
+        int value = 10;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindLast() {
+        int[] input = new int[] {5, 2, 0, 2, 4};
+        int value = 4;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
 }
