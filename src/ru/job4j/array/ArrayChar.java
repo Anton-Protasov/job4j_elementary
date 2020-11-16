b60317486dbc45b18496c79bf3c928ce05036816
Anton-Protasov <89267016232@mail.ru>
2020-11-16 22:27:13 +0300

    6.4. Слово начинается с ... [#395067]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -14,20 +14,14 @@ public class ArrayChar {
         boolean result = true;
         if (word.length > pref.length) {
         for (int i = 0; i < pref.length; i++) {
-            if (word[i] == pref[i]) {
-                result = true;
-            } else {
+            if (word[i] != pref[i]) {
                 result = false;
-                break;
             }
         }
         } else {
             for (int i = 0; i < word.length; i++) {
-                if (word[i] == pref[i]) {
-                    result = true;
-                } else {
+                if (word[i] != pref[i]) {
                     result = false;
-                    break;
                 }
             }
         }
