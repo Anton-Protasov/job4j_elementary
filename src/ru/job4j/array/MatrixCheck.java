a5f0945fe721a4a7318b6252a427de40345b819c
Anton-Protasov <89267016232@mail.ru>
2020-11-17 21:08:52 +0300

    6.7.4. Выигрышные комбинации в сокобан [#395069]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -6,13 +6,23 @@ package ru.job4j.array;
 6.7.2. Моно столбец в матрице. [#395077]
 6.7.3. Массив из диагонали матрицы. [#395078]
 В этом задании вам нужно будет заполнить одномерный массив элементами диагонали из двухмерного массива.
+6.7.4. Выигрышные комбинации в сокобан [#395069]
+Игровое поле представляет собой массив, заполненный символами '_' (подчеркивание) и 'X' - крестик.
+Цель игры - расположить крестики в линию на 5 элементов. Элементы X должны располагаться либо вертикально,
+либо горизонтально.
+Ваша задача - написать метод, который будет проверять, находится ли на поле выигрышная ситуация.
+При любой выигрышной комбинации элементы всегда пересекают диагональ.
+Это дает нам возможность сократить количество проходов.
+Мы определяем координаты элемента в диагонали и проверяем вертикальную и горизонтальную линии.
+Для проверки содержимого строки и столбца нужно использовать методы:
+(monoHorizontal(board, index) || monoVertical(board, index))
  */
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board[row].length; i++) {
-            if (board[row][i] != 'x') {
+            if (board[row][i] != 'X') {
                 result = false;
                 break;
             }
@@ -23,7 +33,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != 'x') {
+            if (board[i][column] != 'X') {
                 result = false;
                 break;
             }
@@ -37,5 +47,18 @@ public class MatrixCheck {
             rsl[i] = board[i][i];
         }
         return rsl;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
+            }
+        }
+        return result;
     }
 }
--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -9,10 +9,10 @@ public class MatrixCheckTest {
     @Test
     public void whenRowOneisNotfilled() {
         char[][] array = new char[][] {
-                                        {'d', 'x', 'a'},
-                                        {'x', 'a', 'f'},
-                                        {'x', 'x', 'x'},
-                                        {'v', 'l', 'x'}
+                                        {'d', 'X', 'a'},
+                                        {'X', 'a', 'f'},
+                                        {'X', 'X', 'X'},
+                                        {'v', 'l', 'X'}
         };
         assertThat(MatrixCheck.monoHorizontal(array, 0), is(false));
     }
@@ -20,10 +20,10 @@ public class MatrixCheckTest {
     @Test
     public void whenRowTwoisNotfilled() {
         char[][] array = new char[][] {
-                {'d', 'x', 'a'},
-                {'x', 'a', 'f'},
-                {'x', 'x', 'x'},
-                {'v', 'l', 'x'}
+                {'d', 'X', 'a'},
+                {'X', 'a', 'f'},
+                {'X', 'X', 'X'},
+                {'v', 'l', 'X'}
         };
         assertThat(MatrixCheck.monoHorizontal(array, 1), is(false));
     }
@@ -31,10 +31,10 @@ public class MatrixCheckTest {
     @Test
     public void whenRowThreeisfilled() {
         char[][] array = new char[][] {
-                {'d', 'x', 'a'},
-                {'x', 'a', 'f'},
-                {'x', 'x', 'x'},
-                {'v', 'l', 'x'}
+                {'d', 'X', 'a'},
+                {'X', 'a', 'f'},
+                {'X', 'X', 'X'},
+                {'v', 'l', 'X'}
         };
         assertThat(MatrixCheck.monoHorizontal(array, 2), is(true));
     }
@@ -42,10 +42,10 @@ public class MatrixCheckTest {
     @Test
     public void whenXIsLast() {
         char[][] array = new char[][] {
-                {'d', 'x', 'a'},
-                {'x', 'a', 'f'},
-                {'x', 'x', 'x'},
-                {'v', 'l', 'x'}
+                {'d', 'X', 'a'},
+                {'X', 'a', 'f'},
+                {'X', 'X', 'X'},
+                {'v', 'l', 'X'}
         };
         assertThat(MatrixCheck.monoHorizontal(array, 3), is(false));
     }
@@ -53,9 +53,9 @@ public class MatrixCheckTest {
     @Test
     public void whenHasMonoVertical() {
         char[][] input = {
-                {' ', ' ', 'x'},
-                {' ', ' ', 'x'},
-                {' ', ' ', 'x'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
         };
         boolean result = MatrixCheck.monoVertical(input, 2);
         assertThat(result, is(true));
@@ -71,5 +71,44 @@ public class MatrixCheckTest {
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expect = {'X', 'X', 'X'};
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataHMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
     }
 }
