988eaa3294eb1ea7c85c9b159c1826a779b23c9d
Anton-Protasov <89267016232@mail.ru>
2021-01-13 00:17:21 +0300

    4. Расстояние между точками в трехмерном пространстве. [#395256]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 /**
  * 2.1. Рефакторинг - Расстояние между точками
+ * 4. Расстояние между точками в трехмерном пространстве
  */
 
 import static java.lang.Math.sqrt;
@@ -17,14 +18,25 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,41 +1,44 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-
-import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 
 public class PointTest {
 
     @Test
-    public void whenX10Y10X21Y21Then1() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 1;
-        int y2 = 1;
+    public void exampleOne2D() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
         double expected = 1.41;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        double out = a.distance(b);
+        assertThat(out, closeTo(expected, 0.1));
     }
 
     @Test
-    public void whenX11Y11X21Y21Then2() {
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 1;
+    public void exampleTwo2D() {
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
         double expected = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        double out = a.distance(b);
+        assertThat(out, closeTo(expected, 0.1));
     }
 
     @Test
-    public void whenX13Y13X21Y21Then3() {
-        int x1 = 3;
-        int y1 = 3;
-        int x2 = 1;
-        int y2 = 1;
-        double expected = 2.82;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void exampleThre3D() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double expected = 1.73;
+        double out = a.distance3d(b);
+        assertThat(out, closeTo(expected, 0.1));
+    }
+
+    @Test
+    public void exampleFoure3D() {
+        Point a = new Point(1, 1, 1);
+        Point b = new Point(1, 1, 1);
+        double expected = 0;
+        double out = a.distance3d(b);
+        assertThat(out, closeTo(expected, 0.1));
     }
 }
